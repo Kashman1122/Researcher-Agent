12d9ee1c25b9f62a20d60f78ea9fc41172a0f316
Kashish <134233339+Kashman1122@users.noreply.github.com>
2024-09-24 19:44:07 +0530

    tasks.py

--- tasks.py
+++ tasks.py
@@ -3,11 +3,17 @@ from agents import dataset_provider_agent
 from tools import tool
 
 dataset_provider=Task(
-description="""
-    Your Task is to provide link of dataset of {input}
+# description="""
+#     Your Task is to provide link of dataset of {input}
+#     """,
+#     expected_output="""
+#     The expected output would be dataset links from google
+#     """,
+    description="""
+    Your Task is to provide top 5 research paper and patent links based on provided {input}
     """,
     expected_output="""
-    The expected output would be dataset links from google
+    The expected output would be patents and research paper links of given idea
     """,
     tools=[tool],
     agent=dataset_provider_agent,
