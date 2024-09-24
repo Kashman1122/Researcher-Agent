918f5e2e9de948af46a07023aef7b9dce70faa38
Kashish <134233339+Kashman1122@users.noreply.github.com>
2024-09-24 19:42:37 +0530

    agents.py

--- agents.py
+++ agents.py
@@ -11,9 +11,16 @@ llm=ChatGoogleGenerativeAI(model="gemini-1.5-pro",
                            temprature=0,
                            google_api_key=google_api_key,)
 dataset_provider_agent = Agent(
-    role="Dataset Specialist and Provider",
-    goal="Deliver high-quality datasets tailored to the user's specific needs, ensuring relevance and readiness for analysis or model training provide only links.",
-    backstory="An experienced virtual data analyst with a broad knowledge base, skilled in sourcing, curating, and optimizing datasets for various applications, from machine learning to business intelligence.",
+    # role="Dataset Specialist and Provider",
+    # goal="Deliver high-quality datasets tailored to the user's specific needs, ensuring relevance and readiness for analysis or model training provide only links.",
+    # backstory="An experienced virtual data analyst with a broad knowledge base, skilled in sourcing, curating, and optimizing datasets for various applications, from machine learning to business intelligence.",
+  role="Innovative Idea Finder",
+    goal="Your goal is to find the top 5 innovative ideas including research paper and patent links based on provided {input}.",
+    backstory=("A creative virtual assistant with a deep understanding of emerging trends and technologies, "
+               "skilled at discovering innovative ideas based on user prompts. With a background in analyzing "
+               "research papers, patents, and industry breakthroughs, this agent excels at delivering actionable "
+               "insights and resources to fuel creativity and innovation. Its mission is to provide users with "
+               "novel and research-driven ideas, helping them stay at the forefront of their fields."),  
     llm=llm,
     tool=[tool],
     allow_delegation=True,
