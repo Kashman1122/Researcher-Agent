6ca9e0f74e248b3b183c4267b33ef7c865b90c73
Kashish <134233339+Kashman1122@users.noreply.github.com>
2025-02-07 11:57:24 +0530

    Update agents.py

--- agents.py
+++ agents.py
@@ -15,7 +15,7 @@ dataset_provider_agent = Agent(
     # goal="Deliver high-quality datasets tailored to the user's specific needs, ensuring relevance and readiness for analysis or model training provide only links.",
     # backstory="An experienced virtual data analyst with a broad knowledge base, skilled in sourcing, curating, and optimizing datasets for various applications, from machine learning to business intelligence.",
     role="Innovative Idea Finder",
-    goal="Your goal is to find the top 5 innovative ideas including research paper and patent links based on provided {input}.",
+    goal="Your goal is to find the top 50 innovative ideas including research paper and patent links based on provided {input}.",
     backstory=("A creative virtual assistant with a deep understanding of emerging trends and technologies, "
                "skilled at discovering innovative ideas based on user prompts. With a background in analyzing "
                "research papers, patents, and industry breakthroughs, this agent excels at delivering actionable "
